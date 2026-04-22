053078f82a2622304ce685d2fd2b33770c8433d4
Paul Walkington <paul.walkington@frasers.group>
2026-04-22 15:46:16 +0100

    Add cloudfront

---
+++ cloudfront.tf
@@ -0,0 +1,46 @@
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = module.elb-wp-frontend.dns_name
+    origin_id   = "alb-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "alb-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "allow-all"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+
+  tags = {
+    Name = "StaticSiteCloudFront"
+  }
+}
--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,10 @@ output "load_balancer_url" {
   value = module.elb-wp-frontend.dns_name
 }
 
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
 output "database_endpoint" {
   value = module.rds_wp_db.cluster_endpoint
 }
